e305488c4e3299a667a0a09107cb2f66c358d59e
Marcelo Costa <marcelo@papel.games>
2021-05-28 01:09:05 -0400

    Cores alteradas da cena 2 para contrastar com cena 1. Prefab criado para transição entre cenas - adicionado na cena 1

---
+++ UnityProject/Assets/Scripts/LoadSceneOnCollision.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnCollision : MonoBehaviour
+{
+
+    public string PlayerTag = "Player";
+    public string SceneName;
+
+    // OnTriggerEnter2D is called when the Collider2D other enters the trigger (2D physics only)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!string.IsNullOrWhiteSpace(SceneName) && collision.CompareTag(PlayerTag))
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+
+
+}
