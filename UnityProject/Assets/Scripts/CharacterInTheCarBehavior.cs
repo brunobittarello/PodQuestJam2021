ad03fc3a3e5debb15380adc859e6821b41cf0a85
CaptainM <marcelo@papel.games>
2021-05-30 02:12:14 -0400

    Restoring driving car scene as Room 30. We may use the driving concept in the future

--- UnityProject/Assets/Scripts/CharacterInTheCarBehavior.cs
+++ UnityProject/Assets/Scripts/CharacterInTheCarBehavior.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class CharacterInTheCarBehavior : CharacterBehaviour
 {
 
-    public Sprite CarSprite;
     public bool InTheCar = false;
-    private Collider2D m_CarCollider;
+    public GameObject Car;
+
+    private SpriteRenderer m_CarSpriteRenderer;
+    private SpriteRenderer m_CharacterSpriteRenderer;
 
     protected override void Start()
     {
         base.Start();
-        m_CarCollider = GetComponent<BoxCollider2D>();
+        m_CarSpriteRenderer = Car.GetComponentInChildren<SpriteRenderer>();
+        m_CharacterSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     protected override Sprite GetMovingSprite(Vector2Int direction)
@@ -38,7 +41,7 @@ public class CharacterInTheCarBehavior : CharacterBehaviour
                 transform.rotation = Quaternion.Euler(0f, 0f, 0f);
             }
 
-            return CarSprite;
+            return m_CarSpriteRenderer.sprite;
         }
         return base.GetMovingSprite(direction);
     }
@@ -47,7 +50,7 @@ public class CharacterInTheCarBehavior : CharacterBehaviour
     {
         if (InTheCar)
         {
-            return CarSprite;
+            return m_CarSpriteRenderer.sprite;
         }
         return base.GetIdleSprite(direction);
     }
@@ -55,7 +58,8 @@ public class CharacterInTheCarBehavior : CharacterBehaviour
     public void GetInTherCar()
     {
         InTheCar = true;
-        m_CarCollider.enabled = true;
+        m_CharacterSpriteRenderer.enabled = false;
+        Car.SetActive(true);
         CanBreak = true;
     }
 
