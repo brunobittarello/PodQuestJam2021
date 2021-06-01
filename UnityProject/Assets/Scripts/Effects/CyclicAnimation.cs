b771a57497f46f9f57168587bbfa32d9ee1f58d2
Bruno Bittarello <brunobitta@hotmail.com>
2021-06-01 19:24:16 -0300

    Animação para a chave

---
+++ UnityProject/Assets/Scripts/Effects/CyclicAnimation.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+class CyclicAnimation : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] spritesAnimation;
+    [SerializeField]
+    float speed;
+
+    SpriteRenderer spriteRenderer;
+    float timer;
+    int index;
+
+    void Awake()
+    {
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (spritesAnimation == null || spritesAnimation.Length == 0)
+            return;
+
+        timer += Time.deltaTime;
+        if (timer < speed)
+            return;
+
+        timer = 0;
+        spriteRenderer.sprite = spritesAnimation[index];
+        index++;
+        if (index == spritesAnimation.Length)
+            index = 0;
+    }
+
+}
