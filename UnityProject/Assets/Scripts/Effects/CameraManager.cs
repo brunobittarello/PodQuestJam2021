e5e714e9542cf6003b2fccba18e070b57d091ec0
Bruno Bittarello <brunobitta@hotmail.com>
2021-06-03 23:53:35 -0300

    Corrigindo problemas com pixel perfect

---
+++ UnityProject/Assets/Scripts/Effects/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.U2D;
+
+class CameraManager : MonoBehaviour
+{
+    PixelPerfectCamera ppcamera;
+
+    private void Awake()
+    {
+        ppcamera = this.gameObject.GetComponent<PixelPerfectCamera>();
+    }
+
+    private void Update()
+    {
+        ppcamera.refResolutionX = Screen.width;
+        ppcamera.refResolutionY = Screen.height;
+        ppcamera.assetsPPU = PPU(Screen.width);
+    }
+
+    int PPU(int width)
+    {
+        if (Screen.width >= 1900) return 64;
+        if (Screen.width >= 1380) return 48;
+        if (Screen.width >= 900) return 32;
+        return 16;
+    }
+}
